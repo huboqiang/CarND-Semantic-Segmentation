3c87012d37ce00feea15fcc8f7f1e0df73a93c03
huboqiang <huboqiang@gmail.com>
2017-09-14 16:25:02 +0800

    grid search for best l2 score, batch-size and learning rate

---
+++ run.sh
@@ -0,0 +1,18 @@
+#python main.py 1e-3 1e-1 1 # run1
+python main.py 1e-3 1e-1 2 # 1505302970.8772528/
+#python main.py 1e-3 1e-2 1 # run3
+python main.py 1e-3 1e-2 2  # 1505303699.9582024/
+#python main.py 1e-3 1e-3 1 # run5
+python main.py 1e-3 1e-3 2  # 1505304417.164329/  best
+python main.py 1e-4 1e-1 1 # 1505305392.1788018/
+python main.py 1e-4 1e-1 2 # 1505306107.8563206/
+python main.py 1e-4 1e-2 1 # 1505307146.8420615/
+python main.py 1e-4 1e-2 2 # 1505307922.0917778/
+python main.py 1e-4 1e-3 1 # 1505309012.1389248/ good
+python main.py 1e-4 1e-3 2 # 1505309825.0854204/ soso
+python main.py 1e-5 1e-1 1 # 1505310942.14949/
+python main.py 1e-5 1e-1 2 # 1505311766.6542244/
+python main.py 1e-5 1e-2 1 
+python main.py 1e-5 1e-2 2
+python main.py 1e-5 1e-3 1
+python main.py 1e-5 1e-3 2
